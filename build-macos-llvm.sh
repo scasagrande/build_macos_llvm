d23800bf98afa35ccf502914acb5d0e9eb636a51
Steven Casagrande <stevencasagrande@gmail.com>
2025-09-06 17:42:17 -0400

    Update for latest toolchains_llvm, simplify build script

--- build-macos-llvm.sh
+++ build-macos-llvm.sh
@@ -1,47 +1,32 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -eux
 
 LLVM_PROJ_DIR=/tmp/llvm-project-$1
 LLVM_TAG="llvmorg-$1"
 OUTPUT_PATH="$2"
-MAJOR_VERSION=$(cut -d '.' -f 1 <<< $1)
+ORIG_DIR="$PWD"
 
-rm -rf $LLVM_PROJ_DIR
-git clone https://github.com/llvm/llvm-project.git --branch $LLVM_TAG --depth 1 $LLVM_PROJ_DIR
-cp update-DistributionExample.cmake.patch $LLVM_PROJ_DIR
+rm -rf "$LLVM_PROJ_DIR"
+git clone https://github.com/llvm/llvm-project.git --branch "$LLVM_TAG" --depth 1 "$LLVM_PROJ_DIR"
+cp update-DistributionExample.cmake.patch "$LLVM_PROJ_DIR"
 
-cd $LLVM_PROJ_DIR
+cd "$LLVM_PROJ_DIR"
 
 git apply update-DistributionExample.cmake.patch
 
 mkdir build && cd build
-cmake -G Ninja -C ../clang/cmake/caches/DistributionExample.cmake ../llvm
+cmake -DCMAKE_INSTALL_PREFIX=../install -G Ninja -C ../clang/cmake/caches/DistributionExample.cmake ../llvm
 ninja stage2-distribution
+ninja stage2-install-distribution
 
-cd tools/clang/stage2-bins
-find lib bin include -type d -name "CMakeFiles" -prune -exec rm -r {} +
-find lib bin include -name cmake_install.cmake -delete
-rm lib/libLLVM*.a lib/libclang*.a lib/liblld*.a
-rm bin/{clang,clang++,clang-cl,clang-cpp}
-rm bin/{ld.lld,ld64.lld,lld-link,wasm-ld}
-rm bin/llvm-ranlib
-
-pushd bin
-
-ln -s clang-${MAJOR_VERSION} clang
-ln -s clang-${MAJOR_VERSION} clang++
-ln -s clang-${MAJOR_VERSION} clang-cl
-ln -s clang-${MAJOR_VERSION} clang-cpp
-
-ln -s lld ld.lld
-ln -s lld ld64.lld
-ln -s lld lld-link
-ln -s lld wasm-ld
-
+cd "${LLVM_PROJ_DIR}/install/bin"
 ln -s llvm-ar llvm-ranlib
 
-popd
+cd "${LLVM_PROJ_DIR}/install"
+tar -cf output.tar bin include lib
+zstd --rm --ultra -T0 -22 output.tar
 
-XZ_OPT="-9e -T0" tar -cJf ${OUTPUT_PATH} bin include lib
+cd "$ORIG_DIR"
+mv "${LLVM_PROJ_DIR}/install/output.tar.zst" "${OUTPUT_PATH}"
 
